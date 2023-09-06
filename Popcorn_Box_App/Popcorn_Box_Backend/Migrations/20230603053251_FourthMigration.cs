84425d69b5c7c2a6ffe308836c5547df90aa702c
Ayush Singh <ayushsing@cybage.com>
2023-09-06 11:05:04 +0530

    NA

---
+++ Popcorn_Box_App/Popcorn_Box_Backend/Migrations/20230603053251_FourthMigration.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Popcorn_Box_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class FourthMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FavoriteMedia",
+                columns: table => new
+                {
+                    FavId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: true),
+                    MediaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FavoriteMedia", x => x.FavId);
+                    table.ForeignKey(
+                        name: "FK_FavoriteMedia_Medias_MediaId",
+                        column: x => x.MediaId,
+                        principalTable: "Medias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FavoriteMedia_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Songs",
+                columns: table => new
+                {
+                    SongsId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    Singers = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    Composer = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    Lyrics = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Year = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    GenreId = table.Column<int>(type: "int", nullable: false),
+                    ClientId = table.Column<int>(type: "int", nullable: false),
+                    Thumbnail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SongUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Songs", x => x.SongsId);
+                    table.ForeignKey(
+                        name: "FK_Songs_Genres_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genres",
+                        principalColumn: "GenreId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Songs_Users_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FavoriteMedia_MediaId",
+                table: "FavoriteMedia",
+                column: "MediaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FavoriteMedia_UserId",
+                table: "FavoriteMedia",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Songs_ClientId",
+                table: "Songs",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Songs_GenreId",
+                table: "Songs",
+                column: "GenreId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FavoriteMedia");
+
+            migrationBuilder.DropTable(
+                name: "Songs");
+        }
+    }
+}
