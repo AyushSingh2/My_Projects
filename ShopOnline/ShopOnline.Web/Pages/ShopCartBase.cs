4f223d620e54f109a8238e83a1efd8651922c879
Ayush Singh <ayushsing@cybage.com>
2023-10-17 17:11:53 +0530

    Shopping cart menu added.

--- ShopOnline/ShopOnline.Web/Pages/ShopCartBase.cs
+++ ShopOnline/ShopOnline.Web/Pages/ShopCartBase.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using ShopOnline.Models.Dtos;
 using ShopOnline.Web.Services.Contracts;
 
@@ -6,6 +7,8 @@ namespace ShopOnline.Web.Pages
 {
     public class ShopCartBase:ComponentBase
     {
+        [Inject]
+        public IJSRuntime Js { get; set; }
         [Inject]
         public IShoppingCartService ShoppingCartService { get; set; }
 
@@ -21,7 +24,7 @@ namespace ShopOnline.Web.Pages
             try
             {
                 ShoppingCartItems = await ShoppingCartService.GetItems(HardCoded.UserId);
-                CalculateCartSummaryTotals();
+                CartChanged();
             }
             catch (Exception ex)
             {
@@ -34,7 +37,7 @@ namespace ShopOnline.Web.Pages
             var cartItemDto = await ShoppingCartService.DeleteItem(id);
 
             RemoveCartItem(id);
-            CalculateCartSummaryTotals();
+            CartChanged();
         }
 
         protected async Task UpdateQtyCartItem_Click(int id, int qty)
@@ -52,7 +55,8 @@ namespace ShopOnline.Web.Pages
                     var returnedUpdateItemDto = await this.ShoppingCartService.UpdateQty(updateItemDto);
 
                     UpdateItemTotalPrice(returnedUpdateItemDto);
-                    CalculateCartSummaryTotals();
+                    CartChanged();
+                    await MakeUpdateQtyButtonVisible(id, false);
                 }
                 else
                 {
@@ -69,6 +73,16 @@ namespace ShopOnline.Web.Pages
 
                 throw;
             }
+        }
+
+        protected async Task UpdateQty_Input(int id)
+        {
+            await MakeUpdateQtyButtonVisible(id, true);
+        }
+
+        private async Task MakeUpdateQtyButtonVisible(int id, bool visible)
+        {
+            await Js.InvokeVoidAsync("MakeUpdateQtyButtonVisible", id, visible);
         }
 
         private void UpdateItemTotalPrice(CartItemDto cartItemDto)
@@ -102,6 +116,11 @@ namespace ShopOnline.Web.Pages
             var cartItemDto = GetCartItem(id);
 
             ShoppingCartItems.Remove(cartItemDto);
+        }
+        private void CartChanged()
+        {
+            CalculateCartSummaryTotals();
+            ShoppingCartService.RaiseEventOnShoppingCartChanged(TotalQuantity);
         }
     }
 }
--- ShopOnline/ShopOnline.Web/wwwroot/js/ShoppingCartFunctions.js
+++ ShopOnline/ShopOnline.Web/wwwroot/js/ShoppingCartFunctions.js
@@ -1,5 +1,5 @@
 ﻿function MakeUpdateQtyButtonVisible(id, visible) {
-    const updateQtyButton = document.querySelector("button[data-itemId='" + id + "'']");
+    const updateQtyButton = document.querySelector("button[data-itemId='" + id + "']");
 
     if (visible) {
         updateQtyButton.style.display = "inline-block";
